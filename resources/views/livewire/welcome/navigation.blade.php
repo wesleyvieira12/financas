7c585559c39fc7c1a71e5fe84fd6b80dbc54c7f9
Wesley Vieira <wesleyvieira12@gmail.com>
2024-06-30 01:32:57 -0300

    install wireui

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="-mx-3 flex flex-1 justify-end">
+<nav class="flex flex-1 justify-end">
     @auth
         <a
             href="{{ url('/dashboard') }}"
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./vendor/wireui/wireui/src/*.php",
+        "./vendor/wireui/wireui/ts/**/*.ts",
+        "./vendor/wireui/wireui/src/WireUi/**/*.php",
+        "./vendor/wireui/wireui/src/Components/**/*.php",
     ],
 
     theme: {
@@ -17,5 +21,11 @@ export default {
         },
     },
 
+    presets: [
+        require("./vendor/wireui/wireui/tailwind.config.js")
+    ],
+
     plugins: [forms],
+
+    darkMode: 'class'
 };
